32b2aaae6dfcaad75d68a18e2c460448604501b2
Manuel Valle <soymanuvalle@gmail.com>
2022-08-07 20:25:15 -0300

    Dependencies update

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule ExqBatch.MixProject do
   def project do
     [
       app: :exq_batch,
-      version: "0.1.1",
-      elixir: "~> 1.8",
+      version: "0.1.2",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Monitors group of Exq jobs",
@@ -31,13 +31,13 @@ defmodule ExqBatch.MixProject do
 
   defp deps do
     [
-      {:exq, "~> 0.14"},
+      {:exq, "~> 0.16"},
       {:elixir_uuid, "~> 1.2"},
-      {:redix, ">= 0.9.0"},
-      {:telemetry, "~> 0.4"},
-      {:jason, "~> 1.0"},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.22.0", only: :dev, runtime: false}
+      {:redix, "~> 1.1"},
+      {:telemetry, "~> 1.1"},
+      {:jason, "~> 1.3"},
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.28.4", only: :dev, runtime: false}
     ]
   end
 end
